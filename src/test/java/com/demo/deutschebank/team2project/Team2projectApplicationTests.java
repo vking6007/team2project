55efeff91e7a5dd2c6d3ce46a1653715887476f9
vking6007 <vaibhavkadam0698@gmail.com>
2025-10-29 21:14:34 +0530

    Initial commit: Spring Boot application with PostgreSQL integration

---
+++ src/test/java/com/demo/deutschebank/team2project/Team2projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.deutschebank.team2project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team2projectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
